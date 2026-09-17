9901456ade84c4306fac53b827a917d66a744124
Aculiecinieks <joao_r_goncalves@estudante.sesisenai.org.br>
2026-09-17 09:34:15 -0300

    ex05 calculador preco duas casas decimais

---
+++ parte1-variaveis/ex05.py
@@ -0,0 +1,7 @@
+preco = float(input("Preco do produto: "))
+quantidade = float(input("quantas vezes o comprou?: "))
+
+total = preco * quantidade
+
+# :.2f duas casas
+print(f"Valor total: {total:.2f}")
