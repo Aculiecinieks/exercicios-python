ed88c30f715ee3d54507a2075f1b838b38fb4ccc
Aculiecinieks <joao_r_goncalves@estudante.sesisenai.org.br>
2026-09-23 20:25:17 -0300

    ex15 contagem de numeros positivos

---
+++ parte3-while/ex15.py
@@ -0,0 +1,9 @@
+positivos = 0
+numero = int(input("Digite um numero: "))
+
+while numero != 0:
+    if numero > 0:
+        positivos += 1
+    numero = int(input("Digite um numero: "))
+
+print(f"Quantidade de numeros positivos: {positivos}")
